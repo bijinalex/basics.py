cafea4d630b6631c3c8c3230428c29f4e2a94f35
bijinalex <bijinalex2028@cs.sjcetpalai.ac.in>
2024-10-18 16:00:18 +0530

    Update stringdemo.py

--- stringdemo.py
+++ stringdemo.py
@@ -1,6 +1,6 @@
 '''
 Authour:bijin
-Date:18-09-2024
+Date:18-10-2024
 Description:Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
 Instructions:
 
