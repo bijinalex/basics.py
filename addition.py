2802d4a83eeb95fe51f7f0595d7e9512e9034197
bijinalex <bijinalex2028@cs.sjcetpalai.ac.in>
2024-10-18 15:58:52 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,34 @@
+'''
+Authour:bijin
+date:18-10-2024
+description: Python program that demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results.
+Instructions:
+
+    Perform addition and division using two integers (a = 10, b = 5).
+    Compare the two integers using the greater-than (>) and equality (==) operators.
+    Use logical operators (and, or) to check conditions.
+    Display the results of all the operations.
+
+Expected Output:
+
+Sum: 15, Division: 2.0
+
+Is a greater than b?: True
+
+Are a and b equal?: False
+
+Logical AND: True
+
+Logical OR: True
+                  '''
+
+
+num1=10
+num2=5
+print("sum:",num1+num2)
+print("Division:",num1/num2)
+print("is num1 greater than num2?",num1>num2)
+print("is num1 greater than num2?",num1==num2)
+print("logical AND:",num1>0)
+print("logical OR:",num2>0)
+print("is num1 greater than num2?",num1<num2)
