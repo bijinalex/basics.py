91b8cc4137757c60ae6c8f80c9ba69fb6cbd9c1f
bijinalex <bijinalex2028@cs.sjcetpalai.ac.in>
2024-10-18 14:27:45 +0530

    Add files via upload

---
+++ bcs.py
@@ -0,0 +1,16 @@
+'''
+Author:bijin
+Date:18-10-2024
+Description: program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
+Expected Output:
+
+Enter your name: John
+
+Enter your age: 25
+
+Hello, John! You are 25 years old.'''
+
+
+name=str(input("Enter your name:"))
+age=int(input("Enter your age:"))
+print("Hello,",name,'! You are ',age," years old.")
