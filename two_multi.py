f38ceb2fcda973430d1b781b62d6c6c3be8d82c1
bijinalex <bijinalex2028@cs.sjcetpalai.ac.in>
2024-11-30 14:49:52 +0530

    Update two_multi.py

--- two_multi.py
+++ two_multi.py
@@ -10,5 +10,8 @@ def positive_multiplication(num1,num2):
         return num1+positive_multiplication(num1,num2-1)
 num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second number:"))
+if num1>0 and num2>0:
+    print("multiplication of two numbers is:",positive_multiplication(num1,num2))
+else:
+    print("Enter a positive number")
 positive_multiplication(num1,num2)
-print("multiplication of two numbers is:",positive_multiplication(num1,num2))
