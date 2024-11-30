a153b7075612d0bf8b4170aee77173b699356dcb
bijinalex <bijinalex2028@cs.sjcetpalai.ac.in>
2024-11-30 16:31:18 +0530

    Add files via upload

---
+++ num_reverse.py
@@ -0,0 +1,12 @@
+'''
+authour-bijin
+date-30\11\2024
+description-Write a Python program to reverse a string.
+Sample String : "1234abcd"
+Expected Output : "dcba4321"
+'''
+
+def reverse(str1):
+    print("reverse of string is:",str[-1::-1])
+str=input("Enter string :")
+reverse(str)
